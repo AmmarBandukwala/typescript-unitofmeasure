7de87c3cea223b453308db89bd37d813b0d9a67c
Ammar Bandukwala <27972300+AmmarBandukwala@users.noreply.github.com>
2020-08-19 12:56:56 -0500

    add test templates

---
+++ test/Temperature.test.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+
+describe("#", function () {
+  // add a test hook
+  beforeEach(function () {
+    // ...some logic before each test is run
+  });
+
+  it("", function () {
+    // do something to test
+  });
+});
